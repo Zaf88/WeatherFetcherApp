06bbb82fdddad0bb252fcc5f2a765422f85d1934
Your Name <applezafar@gmail.com>
2022-07-15 13:04:53 +1000

    создали новый экран WeatherActivity.kt создали WeatherPresenter.kt

--- app/src/main/java/com/example/weatherfetcher/MainActivity.kt
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.weatherfetcher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-val textViewHello = findViewById<TextView>(R.id.tvHello)
-        textViewHello.text = "Hello from code"}}
+        val textViewHello = findViewById<TextView>(R.id.tvHello)
+        textViewHello.text = "Hello from code"
+
+        val btnWeather = findViewById<Button>(R.id.btnWeather)
+        btnWeather.setOnClickListener {
+            Intent(this, WeatherActivity::class.java).also(::startActivity)
+        }
+    }
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/weatherfetcher/WeatherActivity.kt
@@ -0,0 +1,19 @@
+package com.example.weatherfetcher
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class WeatherActivity : AppCompatActivity() {
+    private val weatherPresenter = WeatherPresenter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+
+        val tvTemperature = findViewById<TextView>(R.id.tvTemperature)
+        tvTemperature.text = weatherPresenter.getWeather(UUID.randomUUID().toString())
+    }
+}
