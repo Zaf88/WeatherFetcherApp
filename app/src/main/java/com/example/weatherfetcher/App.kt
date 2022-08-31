684f78f92bd329f7ada10cfb520a1ef1026b672e
Your Name <applezafar@gmail.com>
2022-08-31 23:52:59 +1000

    Темы, стили и атрибуты

--- app/src/main/java/com/example/weatherfetcher/App.kt
+++ app/src/main/java/com/example/weatherfetcher/App.kt
@@ -1,6 +1,7 @@
 package com.example.weatherfetcher
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.weatherfetcher.feature.weather_screen.data.ui.di.weatherScreenModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -15,5 +16,7 @@ import org.koin.core.context.GlobalContext.startKoin
                 androidContext(this@App)
                 modules(weatherScreenModule)
             }
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
         }
     }
--- app/src/main/java/com/example/weatherfetcher/Base/Ext.kt
+++ app/src/main/java/com/example/weatherfetcher/Base/Ext.kt
@@ -1,7 +1,24 @@
 package com.example.weatherfetcher.Base
 
+
+import android.content.Context
+import android.content.res.Configuration
+import android.widget.TextView
+import androidx.annotation.StyleRes
+import androidx.core.widget.TextViewCompat
+
 inline fun <reified T> attempt(func: ()->T): Either<Throwable, T> = try {
     Either.Right(func.invoke())
 } catch (e:Throwable){
     Either.Left(e)
+}
+
+fun TextView.setTextAppearanceCombat(@StyleRes styleRes:Int ) {
+    TextViewCompat.setTextAppearance(this, styleRes)
+}
+
+fun Context.isDarkModeEnabled(): Boolean {
+    return resources.configuration.uiMode and
+            Configuration. UI_MODE_NIGHT_MASK ==Configuration.UI_MODE_NIGHT_YES
+
 }
