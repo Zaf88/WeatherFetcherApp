de2934e19d041552ce8914885082e38a09a2f87c
Your Name <applezafar@gmail.com>
2022-09-04 00:55:47 +1000

    Темы, стили и атрибуты, цвета

--- app/src/main/java/com/example/weatherfetcher/MainActivity.kt
+++ app/src/main/java/com/example/weatherfetcher/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.weatherfetcher
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
@@ -10,9 +11,11 @@ import androidx.core.view.isVisible
 import com.example.weatherfetcher.feature.weather_screen.data.ui.UiEvent
 import com.example.weatherfetcher.feature.weather_screen.data.ui.ViewState
 import com.example.weatherfetcher.feature.weather_screen.data.ui.WeatherScreenViewModel
+import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,9 +27,7 @@ class MainActivity : AppCompatActivity() {
     private val progressBar: ProgressBar by lazy { findViewById(R.id.progressBar) }
     private val btnWeather: Button by lazy { findViewById(R.id.btnWeather) }
     private val collapsingToolbar: CollapsingToolbarLayout by lazy { findViewById(R.id.collapsingToolbar) }
-
-
-
+    private val weatherAppBar: AppBarLayout by lazy { findViewById(R.id.weatherAppBar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,9 +37,14 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, WindActivity::class.java)
             startActivity(intent)
         }
-            viewModel.viewState.observe(this, ::render)
+        viewModel.viewState.observe(this, ::render)
 
-
+        weatherAppBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val percent =
+                (abs(appBarLayout.totalScrollRange + verticalOffset).toFloat()/appBarLayout.totalScrollRange)
+            fabWeather.alpha = percent
+            Log.d("PERCENT", "$percent")
+        })
 
         fabWeather.setOnClickListener {
             viewModel.processUiEvent(UiEvent.OnButtonClicked)
